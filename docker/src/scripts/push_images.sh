c67d5f55b94466196dfb7d5e85f21a662fcc5525
Cedric LOMBARDOT <cedric@donkeycode.com>
2022-05-30 15:27:51 +0200

    Docker

---
+++ docker/src/scripts/push_images.sh
@@ -0,0 +1,28 @@
+cat <<EOF >  ~/project/.circleci/push_images.sh
+#!/bin/bash
+source ~/project/.circleci/determine_env.sh
+
+images=(${IMAGES})
+
+for image in "\${images[@]}"
+do
+    docker pull \${ECR_BASE}/\${image}:\$CIRCLE_SHA1
+    if [ \$? -ne 0 ]; then
+        echo "ERROR DOCKER PULL \${image}"
+        exit 1
+    fi
+
+    docker image tag \${ECR_BASE}/\${image}:\$CIRCLE_SHA1 \${ECR_BASE}/\${image}:\$BUILD_ENV
+    if [ \$? -ne 0 ]; then
+        echo "ERROR DOCKER IMAGE TAG \${image}"
+        exit 1
+    fi
+
+    docker push \${ECR_BASE}/\${image}:\$BUILD_ENV
+    if [ \$? -ne 0 ]; then
+        echo "ERROR DOCKER PUSH \${image}"
+        exit 1
+    fi
+
+done
+EOF
