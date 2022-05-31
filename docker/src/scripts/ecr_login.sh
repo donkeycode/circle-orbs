13d218455a1dce2eeba634ed99f6ba54d9614f93
Cedric LOMBARDOT <cedric@donkeycode.com>
2022-05-31 12:39:25 +0200

    Sudo

--- docker/src/scripts/ecr_login.sh
+++ docker/src/scripts/ecr_login.sh
@@ -10,10 +10,10 @@ if hash apk 2>/dev/null; then
         && apk --purge -v del py-pip \
         && rm -rf /var/cache/apk/*
 else
-    apt-get update
-    apt-get install -y unzip curl
+    sudo apt-get update
+    sudo apt-get install -y unzip curl
     curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-    unzip awscliv2.zip
+    sudo unzip awscliv2.zip
     ./aws/install
 fi
 
