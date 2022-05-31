e2eb97ba2186694c386a30fdaf996b0e08d2f945
Cedric LOMBARDOT <cedric@donkeycode.com>
2022-05-31 14:30:46 +0200

    Fix ecr_login

--- docker/src/scripts/ecr_login.sh
+++ docker/src/scripts/ecr_login.sh
@@ -10,11 +10,11 @@ if hash apk 2>/dev/null; then
         && apk --purge -v del py-pip \
         && rm -rf /var/cache/apk/*
 else
-    sudo apt-get update
+    # sudo apt-get update
     sudo apt-get install -y unzip curl
     curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
     sudo unzip awscliv2.zip
-    ./aws/install
+    sudo ./aws/install -u
 fi
 
 AWS_DEFAULT_REGION=eu-west-1 AWS_ACCESS_KEY_ID=$DOCKER_USER AWS_SECRET_ACCESS_KEY=$DOCKER_PASS aws ecr get-login-password |
